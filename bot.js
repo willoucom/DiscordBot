ecf66e7e963fa9b27465cfca9d3445991ba91f61
Wilfried JEANNIARD <Wilfried JEANNIARD>
2016-05-19 17:42:23 +0200

    cleanup

--- bot.js
+++ bot.js
@@ -30,14 +30,6 @@ try {
 	process.exit();
 }
 
-// Get Twitch API data
-try {
-	var url = "https://api.twitch.tv/kraken/channels/lastwolfplay";
-} catch (e){
-	console.log("Please verify your username to twitch api");
-	process.exit();
-}
-
 // File exists
 var fs = require('fs');
 function fileExists(filePath) {
@@ -76,76 +68,11 @@ mybot.on("message", function(message) {
 		}
 	}
 
-
-
 	// Commande List
 	if(input === "!command") {
-		mybot.reply(message, "Hey voici la liste des choses que je peux faire : !rules \n !social \n !thedivision \n !farmroad \n !diablo3 \n !drahquebible \n !stream \n !donnation \n !hardware \n !nvidia \n !planning \n !tasaka \n !lastwolfplay \n !thedivision");
+		mybot.reply(message, "Hey voici la liste des choses que je peux faire : !hello \n !twitter");
 	}
 
-	// Commande de bienvenu
-	if(input === "yop tout le monde") {
-		mybot.reply(message, "Hey coucou toi bienvenu");
-	}
-	if(input === "yop") {
-		mybot.reply(message, "Hey coucou toi bienvenu");
-	}
-	if(input === "salut la meute") {
-		mybot.reply(message, "Hey coucou toi bienvenu");
-	}
-	if(input === "bonjour") {
-		mybot.reply(message, "Hey coucou toi bienvenu");
-	}
-	if(input === "hi") {
-		mybot.reply(message, "Hey coucou toi bienvenu");
-	}
-	if(input === "salut") {
-		mybot.reply(message, "Hey coucou toi bienvenu");
-	}
-
-	// Commande Bot Twitch
-	if(input === "!rules") {
-		mybot.sendMessage(message, "Bienvenu(e)s dans la meute ! \n Ici les deux seules règles qui prévalent sont le respect des autres et de l'orthographe. \n Les traits d'esprits sont vivement encouragés et n'oubliez pas qu'en écrivant sur le tchat vos écrits sont publics ! \n Bon jeu");
-	}
-	if(input === "!social") {
-		mybot.sendMessage(message, "Le dernier loup sur la toile c'est par là : https://twitter.com/lastwolfplays \n https://www.facebook.com/lastwolfplays \n https://www.youtube.com/channel/UC_D3dHEqqZrx_k82v5lJglQ");
-	}
-	if(input === "!thedivision") {
-		mybot.sendMessage(message, "Un des gdoc de référence sur le jeu : https://goo.gl/aLkMld  \n Map de la DZ complète avec filtres  http://goo.gl/JhFEGU \n le gdoc de pandé avec les builds dps et supports => https://goo.gl/zuFN36");
-	}
-	if(input === "!farmroad") {
-		mybot.sendMessage(message, "https://youtu.be/qkAqSGHir30");
-	}
-	if(input === "!diablo3") {
-		mybot.sendMessage(message, "Voici la liste de mes personnages : http://eu.battle.net/d3/fr/profile/Lastwolf-2869/hero/54563586");
-	}
-	if(input === "!drahquebible") {
-		mybot.sendMessage(message, "Voici le Gdoc de référence sur le start de saison sur Diablo 3 : https://docs.google.com/spreadsheets/d/1gCq8ihJBcYDZpPICFqA407fL0Fl_K9PMBp83npy5DcM/htmlview?usp=sharing&sle=true");
-	}
-	if(input === "!stream") {
-		mybot.sendMessage(message, "hey voici le stream de lastwolf : https://www.twitch.tv/lastwolfplay");
-	}
-	if(input === "!nvidia") {
-		mybot.sendMessage(message, "hey Lastwolfplay lance son stream sur NvidiaFrance : https://www.twitch.tv/nvidiafrance");
-	}
-	if(input === "!donnation") {
-		mybot.sendMessage(message, "hey si toi aussi tu veux soutenir le stream de lastwolf : https://www.twitchalerts.com/donate/lastwolfplay \n les donnations ne sont pas obligatoire mais elles aident pour l'amélioration du stream");
-	}
-	if(input === "!hardware") {
-		mybot.sendFile(message, "./Assets/hardware.jpg");
-	}
-
-	// Parse json API Twitch
-	if (input === "!planning") {
-		request({
-			url: url,
-			json: true
-		}, function (error, response, body) {
-			if (!error && response.statusCode === 200) {
-				mybot.sendFile(message, body.video_banner);
-			}
-		})
-	}
 
 // bot function close
 });
@@ -154,7 +81,7 @@ mybot.on('ready', () => {
 	console.log("Bot is ready.");
 });
 
-// login Discord TasakaBot
+// login Discord
 mybot.loginWithToken(AuthDetails.discord.token).then(bot_success).catch(bot_error);
 
 function bot_success(token){
